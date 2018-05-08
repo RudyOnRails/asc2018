178265666d01c8bf9acdc752a6f0223ec596ca77
Kevin Musiorski <kmusio@artic.edu>
2018-05-07 21:58:08 -0500

    mount routes so we can get to web console

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/jobs'
   
   mount Riiif::Engine => 'images', as: :riiif if Hyrax.config.iiif_image_server?
   mount Blacklight::Engine => '/'
